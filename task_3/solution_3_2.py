7f0460b152fca107d7410b3a18df1ba9adfc53e4
kov-t-a <72095872+kov-t-a@users.noreply.github.com>
2020-10-29 22:06:05 +0300

    Update solution_3_2.py

--- task_3/solution_3_2.py
+++ task_3/solution_3_2.py
@@ -1,6 +1,8 @@
-# your code here
-for smth in 'a'*10:
-    # your code here
-    user_input = input()
-# your code here
+alphabet = str()
+i=0
+for i in range(10):
+  a = input('Введите последовательность букв')
+  for i in a:
+      if i not in alphabet:
+        alphabet = alphabet + i
 print(alphabet)
