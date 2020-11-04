fb3374e86f14805f3d72a107280be39e7e8de3af
kov-t-a <72095872+kov-t-a@users.noreply.github.com>
2020-11-04 21:06:37 +0300

    Update solution_4_2.py

--- task_4/solution_4_2.py
+++ task_4/solution_4_2.py
@@ -1,6 +1,21 @@
-# your code here
-for smth in 'a'*10:
-    user_input = input()
-    # your code here
-    if my_cool_condition == True:
-        print(form, lemma)
+n=0
+while n<=10:
+    a=input()
+form = str()
+lemma = str()
+if a[0] != '\t' and a[0] != " " and form[1] != "lemma":
+  while a[1]!=int:
+    n=n+1
+  while a[2]!='\t':
+    n=n+1
+  while a[4]!='\t':
+    n=n+1
+  while a[6]!='\t':
+    n=n+1
+  while a[form]!='\t':
+     form=form+1
+  while a[lemma]!='\t':
+      lemma=lemma+1
+if lemma!=form:
+  print(form, lemma)
+  
